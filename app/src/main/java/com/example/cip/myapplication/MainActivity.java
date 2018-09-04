ae412423f3423cb4db7918e41e0f29fde80fe295
Johanna Dangel <32933310+JohannaDangel@users.noreply.github.com>
2018-09-04 10:35:52 +0200

    Burger-Menü
    Burger-Button wird angezeigt

--- app/src/main/java/com/example/cip/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cip/myapplication/MainActivity.java
@@ -1,29 +1,38 @@
 package com.example.cip.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
 
 
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
     //implements NavigationView.OnNavigationItemSelectedListener {
     //ALLE KOMMENTARE STAMMEN AUS EINER VORLAGE AUS DEM INTERNET. EVENTUELL BRAUCHEN WIR SIE JA NOCH
 
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-/*
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-*/
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
             @Override
@@ -60,19 +69,16 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-               // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-               // drawer.closeDrawer(GravityCompat.START);
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
 
 
         });
-
     }
 
 
-
-/*
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -85,27 +91,21 @@ public class MainActivity extends AppCompatActivity {
 
     // VORLAGE VON ANDROID STUDIO
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        /*int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+       /* if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
-        return super.onOptionsItemSelected(item);*/
+        //return super.onOptionsItemSelected(item);
 
         /*int id = item.getItemId();
 
@@ -113,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             do sth.
         }
 
-        return super.onOptionsItemSelected(item);
-    }*/
+        return super.onOptionsItemSelected(item);*/
+    //}
 
 }
