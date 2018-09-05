978c7a85c0b760959ccfabbda856da2d6e3d3eb1
Johanna Dangel <32933310+JohannaDangel@users.noreply.github.com>
2018-09-05 15:48:57 +0200

    Screenshot teilen

--- app/src/main/java/com/example/cip/myapplication/GameEnd.java
+++ app/src/main/java/com/example/cip/myapplication/GameEnd.java
@@ -1,16 +1,27 @@
 package com.example.cip.myapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GameEnd extends AppCompatActivity {
 
     Button bShare,
         bNewGame;
+    private File imagePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +34,9 @@ public class GameEnd extends AppCompatActivity {
         bShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(Intent.ACTION_SEND);
-                myIntent.setType("text/plain");
-                String shareBody = "You Body Here";
-                String shareSub = "Your Subjet here";
-                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
-                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
-                startActivity(Intent.createChooser(myIntent, "Erfolg teilen"));
+                Bitmap bitmap = takeScreenshot();
+                saveBitmap(bitmap);
+                shareIt();
             }
         });
 
@@ -43,4 +50,39 @@ public class GameEnd extends AppCompatActivity {
             }
         });
     }
+
+    public Bitmap takeScreenshot() {
+        View rootView = findViewById(android.R.id.content).getRootView();
+        rootView.setDrawingCacheEnabled(true);
+        return rootView.getDrawingCache();
+    }
+
+    public void saveBitmap(Bitmap bitmap) {
+        imagePath = new File(Environment.getExternalStorageDirectory() + "/screenshot.png");
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(imagePath);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e("GREC", e.getMessage(), e);
+        } catch (IOException e) {
+            Log.e("GREC", e.getMessage(), e);
+        }
+    }
+
+    private void shareIt() {
+        Uri uri = Uri.fromFile(imagePath);
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("image/*");
+        //String shareBody = "In Tweecher, My highest score with screen shot";
+        //sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Tweecher score");
+        //sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        //sharingIntent.putExtra(Intent.EXTRA_TITLE,  shareBody);
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        startActivity(Intent.createChooser(sharingIntent, "Teilen über"));
+    }
+
 }
