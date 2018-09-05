e894801c42b5a2720eda24337d55c24d8e3a16ab
Johanna Dangel <32933310+JohannaDangel@users.noreply.github.com>
2018-09-05 12:54:33 +0200

    Teilen von Erfolgen

--- app/src/main/java/com/example/cip/myapplication/GameEnd.java
+++ app/src/main/java/com/example/cip/myapplication/GameEnd.java
@@ -1,11 +1,15 @@
 package com.example.cip.myapplication;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameEnd extends AppCompatActivity {
 
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,19 @@ public class GameEnd extends AppCompatActivity {
 
         MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.win);
         mp.start();
+
+        bt = (Button) findViewById(R.id.buttonShare);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "You Body Here";
+                String shareSub = "Your Subjet here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "Erfolg teilen"));
+            }
+        });
     }
 }
