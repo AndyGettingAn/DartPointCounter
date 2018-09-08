deb9d5021f126999815f2127fca290c20a979cab
Johanna Dangel <32933310+JohannaDangel@users.noreply.github.com>
2018-09-08 13:02:36 +0200

    Codequalität Fehler:  drawer.setDrawerListener(toggle);

--- app/src/main/java/com/example/cip/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cip/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.cip.myapplication;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,12 +11,6 @@ import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.content.Intent;
-import android.widget.Toast;
-
-
-//public class MainActivity extends AppCompatActivity {
-//implements NavigationView.OnNavigationItemSelectedListener {
-//ALLE KOMMENTARE STAMMEN AUS EINER VORLAGE AUS DEM INTERNET. EVENTUELL BRAUCHEN WIR SIE JA NOCH
 
 public class MainActivity extends AppCompatActivity{
 
@@ -26,28 +20,16 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-
-
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 Intent intent = new Intent();
                 if (id == R.id.nav_settings) {
@@ -62,11 +44,8 @@ public class MainActivity extends AppCompatActivity{
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
-
-
         });
     }
-
 
     @Override
     public void onBackPressed(){
@@ -77,32 +56,4 @@ public class MainActivity extends AppCompatActivity{
             super.onBackPressed();
         }
     }
-
-    // VORLAGE VON ANDROID STUDIO
-
-
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-       /* if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-    //return super.onOptionsItemSelected(item);
-
-        /*int id = item.getItemId();
-
-        if (id == R.id.action_next) {
-            do sth.
-        }
-
-        return super.onOptionsItemSelected(item);*/
-    //}
-
 }
